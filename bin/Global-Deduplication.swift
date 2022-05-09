77cbf0d38f61c3635599071a2bf58631c52029e1
ShikiSuen <shikisuen@outlook.com>
2022-05-09 11:03:36 +0800

    Clang-Format update - 20220509

--- bin/Global-Deduplication.swift
+++ bin/Global-Deduplication.swift
@@ -27,22 +27,22 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import Foundation
 
 extension String {
-  mutating func regReplace(pattern: String, replaceWith: String = "") {
-    do {
-      let regex = try NSRegularExpression(
-        pattern: pattern, options: [.caseInsensitive, .anchorsMatchLines]
-      )
-      let range = NSRange(startIndex..., in: self)
-      self = regex.stringByReplacingMatches(
-        in: self, options: [], range: range, withTemplate: replaceWith
-      )
-    } catch { return }
-  }
+    mutating func regReplace(pattern: String, replaceWith: String = "") {
+        do {
+            let regex = try NSRegularExpression(
+                pattern: pattern, options: [.caseInsensitive, .anchorsMatchLines]
+            )
+            let range = NSRange(startIndex..., in: self)
+            self = regex.stringByReplacingMatches(
+                in: self, options: [], range: range, withTemplate: replaceWith
+            )
+        } catch { return }
+    }
 }
 
 func getDocumentsDirectory() -> URL {
-  let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-  return paths[0]
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
 }
 
 let urlCHSforMCBP = "../components/chs/phrases-mcbp-chs.txt"
@@ -57,66 +57,66 @@ var textCHT = ""
 
 // 档案载入
 do {
-  textCHS += "@# phrases-moe-chs.txt\n"
-  textCHS += try String(contentsOfFile: urlCHSforMOE, encoding: .utf8)
-  textCHS += "\n@# phrases-mcbp-chs.txt\n"
-  textCHS += try String(contentsOfFile: urlCHSforMCBP, encoding: .utf8)
-  textCHS += "\n@# phrases-moe-vchewing.txt\n"
-  textCHS += try String(contentsOfFile: urlCHSforVCHEW, encoding: .utf8)
+    textCHS += "@# phrases-moe-chs.txt\n"
+    textCHS += try String(contentsOfFile: urlCHSforMOE, encoding: .utf8)
+    textCHS += "\n@# phrases-mcbp-chs.txt\n"
+    textCHS += try String(contentsOfFile: urlCHSforMCBP, encoding: .utf8)
+    textCHS += "\n@# phrases-moe-vchewing.txt\n"
+    textCHS += try String(contentsOfFile: urlCHSforVCHEW, encoding: .utf8)
 } catch { print("Exception happened when reading raw CHS data.") }
 
 do {
-  textCHT += "@# phrases-moe-cht.txt\n"
-  textCHT += try String(contentsOfFile: urlCHTforMOE, encoding: .utf8)
-  textCHT += "\n@# phrases-mcbp-cht.txt\n"
-  textCHT += try String(contentsOfFile: urlCHTforMCBP, encoding: .utf8)
-  textCHT += "\n@# phrases-moe-vchewing.txt\n"
-  textCHT += try String(contentsOfFile: urlCHTforVCHEW, encoding: .utf8)
+    textCHT += "@# phrases-moe-cht.txt\n"
+    textCHT += try String(contentsOfFile: urlCHTforMOE, encoding: .utf8)
+    textCHT += "\n@# phrases-mcbp-cht.txt\n"
+    textCHT += try String(contentsOfFile: urlCHTforMCBP, encoding: .utf8)
+    textCHT += "\n@# phrases-moe-vchewing.txt\n"
+    textCHT += try String(contentsOfFile: urlCHTforVCHEW, encoding: .utf8)
 } catch { print("Exception happened when reading raw CHT data.") }
 
 // Regex Pre-Processing
-textCHT.regReplace(pattern: #"( +|　+| +|\t+)+"#, replaceWith: " ")  // Concatenating Spaces
-textCHT.regReplace(pattern: #"(^ | $)"#, replaceWith: "")  // 去除行尾行首空格
-textCHT.regReplace(pattern: #"(\f+|\r+|\n+)+"#, replaceWith: "\n")  // CR & Form Feed to LF, 且去除重複行
-textCHS.regReplace(pattern: #"( +|　+| +|\t+)+"#, replaceWith: " ")  // Concatenating Spaces
-textCHS.regReplace(pattern: #"(^ | $)"#, replaceWith: "")  // 去除行尾行首空格
-textCHS.regReplace(pattern: #"(\f+|\r+|\n+)+"#, replaceWith: "\n")  // CR & Form Feed to LF, 且去除重複行
+textCHT.regReplace(pattern: #"( +|　+| +|\t+)+"#, replaceWith: " ") // Concatenating Spaces
+textCHT.regReplace(pattern: #"(^ | $)"#, replaceWith: "") // 去除行尾行首空格
+textCHT.regReplace(pattern: #"(\f+|\r+|\n+)+"#, replaceWith: "\n") // CR & Form Feed to LF, 且去除重複行
+textCHS.regReplace(pattern: #"( +|　+| +|\t+)+"#, replaceWith: " ") // Concatenating Spaces
+textCHS.regReplace(pattern: #"(^ | $)"#, replaceWith: "") // 去除行尾行首空格
+textCHS.regReplace(pattern: #"(\f+|\r+|\n+)+"#, replaceWith: "\n") // CR & Form Feed to LF, 且去除重複行
 
 // 转成 Vector
 var arrData = textCHS.components(separatedBy: "\n")
 var varLineData = ""
 var strProcessed = ""
 for lineData in arrData {
-  varLineData = lineData
-  varLineData.regReplace(pattern: "^#.*$", replaceWith: "")  // Make Comment Lines Empty
-  strProcessed += varLineData
-  strProcessed += "\n"
+    varLineData = lineData
+    varLineData.regReplace(pattern: "^#.*$", replaceWith: "") // Make Comment Lines Empty
+    strProcessed += varLineData
+    strProcessed += "\n"
 }
 
 arrData = strProcessed.components(separatedBy: "\n")
-let arrCHS = Array(NSOrderedSet(array: arrData).array as! [String])  // Deduplication
+let arrCHS = Array(NSOrderedSet(array: arrData).array as! [String]) // Deduplication
 
 arrData = textCHT.components(separatedBy: "\n")
 varLineData = ""
 strProcessed = ""
 for lineData in arrData {
-  varLineData = lineData
-  varLineData.regReplace(pattern: "^#.*$", replaceWith: "")  // Make Comment Lines Empty
-  strProcessed += varLineData
-  strProcessed += "\n"
+    varLineData = lineData
+    varLineData.regReplace(pattern: "^#.*$", replaceWith: "") // Make Comment Lines Empty
+    strProcessed += varLineData
+    strProcessed += "\n"
 }
 
 arrData = strProcessed.components(separatedBy: "\n")
-let arrCHT = Array(NSOrderedSet(array: arrData).array as! [String])  // Deduplication
+let arrCHT = Array(NSOrderedSet(array: arrData).array as! [String]) // Deduplication
 
 // Print Out
 for lineData in arrCHT {
-  varLineData = lineData
-  print(varLineData)
+    varLineData = lineData
+    print(varLineData)
 }
 
 print("@@@@@@@@@@@@@@@@@@@@")
 for lineData in arrCHS {
-  varLineData = lineData
-  print(varLineData)
+    varLineData = lineData
+    print(varLineData)
 }
